db084dfc1942e5085d1ccbc3bd671a8dbc7507f1
Vladyslav <vkadyrov@protonmail.com>
2023-03-04 14:43:17 -0500

    commit tree 0ad5f4cb9c177f2ea6b8481f0740947ba37b285c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,29 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
-    { message: "Hello world" }.to_json
+    { message: 'Hello world' }.to_json
   end
 
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name],
+            },
+          },
+        },
+      },
+    )
+  end
 end
